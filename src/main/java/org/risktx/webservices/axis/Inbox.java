1a6a74dec329695948a40f4b03b4ece199e4fa58
amoss <am@risktx.org>
2009-02-06 12:36:31 +0800

    work in progress, commented out the Mule specifics to allow complete build
    Signed-off-by: pfuller <paul.fuller.uk@googlemail.com>
    
    Signed-off-by: Andrew Moss <am@risktx.org>

--- src/main/java/org/risktx/webservices/axis/Inbox.java
+++ src/main/java/org/risktx/webservices/axis/Inbox.java
@@ -5,12 +5,10 @@ import org.apache.axiom.om.impl.llom.util.AXIOMUtil;
 import org.apache.axiom.om.OMElement;
 import org.apache.axiom.soap.SOAPEnvelope;
 import org.apache.axis2.context.MessageContext;
-import org.mule.api.MuleException;
-import org.mule.module.client.MuleClient;
 
 public class Inbox {
 
-  public OMElement PostRq() throws XMLStreamException, MuleException {
+  public OMElement PostRq() throws XMLStreamException, Exception {
     //retrieve the context and SOAP envelope (contains the AMS message)
     MessageContext context = MessageContext.getCurrentMessageContext();
     SOAPEnvelope envelope = context.getEnvelope();
@@ -20,9 +18,9 @@ public class Inbox {
     message.setAttachmentMap(context.getAttachmentMap());
     message.setSoapEnvelopeIn(envelope.toString());
 
-    //connect to Mule and pass in the message using the VM transport
+    /* //connect to Mule and pass in the message using the VM transport
     MuleClient client = new MuleClient();
-    client.send("vm://risktx.receive.vm", message, null);
+    client.send("vm://risktx.receive.vm", message, null); */
     
     //create the SOAP response from the string returned by Mule (using the Axiom Object Model)
     return AXIOMUtil.stringToOM(message.getSoapEnvelopeOut());
