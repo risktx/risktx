0a2eb224d6dedd803ab509956e2c67bfcab05c70
mosstache <am@risktx.org>
2009-11-02 17:59:51 +0000

    More scala doc

--- src/main/scala/org/risktx/model/Message.scala
+++ src/main/scala/org/risktx/model/Message.scala
@@ -4,7 +4,7 @@ import net.liftweb.mapper._
 import scala.xml._
 
 /**
-* Message Class which encapsulates a SOAP message
+* Message entity which encapsulates a RiskTx message exchange
 **/
 class Message extends LongKeyedMapper[Message] with IdPK {
   def getSingleton = Message
@@ -108,8 +108,8 @@ class Message extends LongKeyedMapper[Message] with IdPK {
 }
 
 /**
-  * Implementation of Message with Helper methods
-  **/
+* Message companion object with Helper methods
+**/
 object Message extends Message with LongKeyedMetaMapper[Message] with CRUDify[Long, Message] {
   /**
   * Helper method to find messages for a particular Trading Party
