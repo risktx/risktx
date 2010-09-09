f0a7da4790eb86ac0c6f2ab3508e36db455d05e8
mosstache <andrmos@gmail.com>
2010-09-09 22:31:01 +0100

    Templates can be loaded from files - allows default templates to be bundled with the app code

--- src/main/scala/org/risktx/repository/TemplateRepository.scala
+++ src/main/scala/org/risktx/repository/TemplateRepository.scala
@@ -1,11 +1,26 @@
 package org.risktx.repository
 
 import org.risktx.domain.model.messaging._
+//import net.liftweb.http.LiftRules
+// import net.liftweb.common._
 
 object TemplateRepository {
 
-  def findTemplate(standard: String, version: String): Template = {
-    Template("Here's a ${value1}")
+  def findRequestTemplate(standard: String, version: String): Template = {
+    val template = "/request/" + standard + "_" + version + ".tmpl"
+    loadTemplate(template)
+  }
+
+  def findResponseTemplate(standard: String, version: String): Template = {
+    val template = "/response/" + standard + "_" + version + ".tmpl"
+    loadTemplate(template)
+  }
+
+  private def loadTemplate(template: String): Template = {
+    val stream = this.getClass.getResourceAsStream(template)
+    val content = io.Source.fromInputStream(stream).getLines.mkString
+
+    Template(content)
   }
 
 }
